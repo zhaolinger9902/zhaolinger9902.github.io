6068613de2d4a196d9138bfc0f38979312c69a2f
root <root@localhost.localdomain>
2024-10-04 23:41:55 +0800

    .

--- _next/static/chunks/app/[lang]/page-0e06f08c180faf7a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[495],{},function(n){n.O(0,[957,51,505,950,240,699,706,259,675,49,536,995,971,23,744],function(){return n(n.s=6995)}),_N_E=n.O()}]);
